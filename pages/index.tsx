6af3334b1b5d30cb26f71f70941df3c32f4ed279
KaramveerSidhu <karamveersidhu14@gmail.com>
2023-09-01 10:50:41 +0530

    chore: restrucutre home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,4 @@
-import styles from "@styles/Home.module.css";
-import { Inter } from "next/font/google";
 import Head from "next/head";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
@@ -13,7 +9,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={`${styles.main} ${inter.className}`}>Hi!</main>
+      <div> Home </div>
     </>
   );
 }
