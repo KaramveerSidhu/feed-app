343bdb2288f12bd631c84ac47983e2838a81b4d6
KaramveerSidhu <karamveersidhu14@gmail.com>
2023-09-01 11:00:37 +0530

    data: add posts

---
+++ constants/posts.ts
@@ -0,0 +1,24 @@
+export const POSTS = [
+  {
+    id: "669e72f6-e8f6-4b3d-86ec-c23cb51f236b",
+    profileImgUrl: "/images/profiles/theresa.png",
+    userName: "Theresa Webb",
+    createdAt: "5mins ago",
+    content:
+      "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.",
+    moodEmoji: "👋",
+    commentsCount: 24,
+    isEdited: false,
+  },
+  {
+    id: "0f485023-d3bd-4f70-b4b9-ba397eba83c3",
+    profileImgUrl: "/images/profiles/marvin.png",
+    userName: "Marvin McKinney",
+    createdAt: "8mins ago",
+    content:
+      "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.",
+    moodEmoji: "😞",
+    commentsCount: 32,
+    isEdited: true,
+  },
+];
